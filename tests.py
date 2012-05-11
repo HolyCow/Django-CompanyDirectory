3b508abb692e00c373895346156bdcb1eba1bc01
HolyCow <mike.beasterfeld@gmail.com>
2012-05-10 23:02:08 -0500

    Change test to load fixtures

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-class PollsViewsTestCase(TestCase):
+class CompanydirectoryViewsTestCase(TestCase):
+    fixtures = ['initial_data.json']
     def test_index(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
